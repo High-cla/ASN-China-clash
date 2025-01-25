71b97ad63bda94fef7b25ab0072db3a08d5e49af
High-cla <114676697+High-cla@users.noreply.github.com>
2025-01-25 19:14:42 +0800

    Add files via upload

---
+++ scripts/IPlist_to_yaml.py
@@ -0,0 +1,8 @@
+with open("IP.China.list", 'r') as f:
+    iplist = f.readlines()
+
+output = 'payload:\n'
+for line in iplist:
+    output += "  - '" + line.strip() + "'\n"
+with open('IP.China.yaml', 'w') as f:
+    f.write(output)
