4c9da2c2e28abb6b3d68dc22e8e9d144f6ad437f
High-cla <114676697+High-cla@users.noreply.github.com>
2025-02-28 21:55:53 +0800

    更新 IPlist_to_CIDR.py

--- scripts/IPlist_to_CIDR.py
+++ scripts/IPlist_to_CIDR.py
@@ -1,9 +1,9 @@
 with open('IP.China.list', 'r') as f, open('IP.China.txt', 'w') as o:
     lines = f.readlines()
     for line in lines:
-        line = line.strip()  
+        line = line.strip()
         if ":" in line:
-            new_line = "IP-CIDR6," + line + "\n"
+            new_line = "IP-CIDR6," + line + ",no-resolve\n"
         else:
-            new_line = "IP-CIDR," + line + "\n"
+            new_line = "IP-CIDR," + line + ",no-resolve\n"
         o.write(new_line)
