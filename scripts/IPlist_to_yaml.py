8408b2bdfca859d2549a11eaf3990d61705922f8
High-cla <114676697+High-cla@users.noreply.github.com>
2025-06-23 14:18:39 +0800

    Update IPlist_to_yaml.py

--- scripts/IPlist_to_yaml.py
+++ scripts/IPlist_to_yaml.py
@@ -3,6 +3,6 @@ with open("IP.China.txt", 'r') as f:
 
 output = 'payload:\n'
 for line in iplist:
-    output += "  - '" + line.strip() + ",no-resolve'\n"
+    output += "  - " + line.strip() + "'\n"
 with open('IP.China.yaml', 'w') as f:
     f.write(output)
